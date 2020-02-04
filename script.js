5a2725e064e43945e04ebb5952f5945cdc91c208
LightDev1 <karumasanmy@gmail.com>
2020-02-05 02:30:25 +0400

    Day 3

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
     palette = document.querySelector('.palette'),
-    fillButton = document.querySelector('.fill');
-
+    fillButton = document.querySelector('.fill'),
+    input = document.querySelector('.numbers'),
+    eraser = document.querySelector('.eraser'),
+    currentColor = document.querySelector('.current__color'),
+    pencil = document.querySelector('.pencil'),
+    brush = document.querySelector('.brush');
+    
 let isMouseDown = false;
-let coords = [];
-let radius = 5;
 let color = 'black';
+let isPressed = false;
+let isCleanPressed = false;
+let cleanColor = 'white';
+let prevElement;
 
 canvas.width = 1100;
-canvas.height = 550;
+canvas.height = 555;
+
+const toFocus = (elem, condition) => {
+    if (condition) {
+        elem.style.backgroundColor = '#FFC45D';
+        elem.style.border = '1px solid #FFC45D';
+    }
+
+    
+    if (prevElement && !(elem === prevElement)) {
+      prevElement.style.backgroundColor = '';
+      prevElement.style.border = '';
+    }
+
+    prevElement = elem;
+
+}
 
 canvas.addEventListener('mousedown', () => {
   isMouseDown = true;
@@ -18,8 +41,11 @@ canvas.addEventListener('mousedown', () => {
 canvas.addEventListener('mouseup', () => {
   isMouseDown = false;
   ctx.beginPath();
-  coords.push('mouseup');
 }); 
+
+const showCurrentColor = () => {
+  currentColor.style.backgroundColor = color;
+};
 
 palette.addEventListener('click', (event) => {
     let target = event.target;
@@ -31,6 +57,7 @@ palette.addEventListener('click', (event) => {
     let elemColor = target.id;
     color = `${elemColor}`;
     ctx.fillStyle = color;
+    showCurrentColor();
 
 });
 
@@ -40,17 +67,38 @@ const fill = () => {
   
     ctx.beginPath();
     ctx.fillStyle = color;
+    isCleanPressed = false;
 };
 
-ctx.lineWidth = radius * 2;
+const toClean = () =>  {
+ isCleanPressed = true;
+  showCurrentColor();
+  toFocus(eraser, true);
+};
+
+
+const drawPencil = () => {
+  toFocus(pencil, true);
+  input.value = 1;
+  isCleanPressed = false;
+};
+
+const drawBrush = () => {
+  input.value = 5;
+  toFocus(brush, true);
+  isCleanPressed = false;
+};
 
 canvas.addEventListener('mousemove', (event) => {
   if (isMouseDown) {
 
-    coords.push([event.clientX, event.clientY]);
-   
+    let radius = input.value;
+    ctx.lineWidth = radius * 2;
+  
+    ctx.fillStyle =  (isCleanPressed) ? 'white' : color;
+
     ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.strokeStyle = color;
+    ctx.strokeStyle = (isCleanPressed) ? 'white' : color;
     ctx.stroke();
 
     ctx.beginPath();
@@ -61,10 +109,6 @@ canvas.addEventListener('mousemove', (event) => {
     ctx.moveTo(event.offsetX, event.offsetY);
   }
 });
-
-const save = () => {
-    localStorage.setItem('coords', JSON.stringify(coords));
-};
 
 const clear = () => {
   ctx.fillStyle = 'white';
@@ -74,55 +118,25 @@ const clear = () => {
   ctx.fillStyle = color;
 };
 
-const replay = () => {
-  let timer = setInterval(() => {
-    if (!coords.length) {
-      clearInterval(timer);
-      ctx.beginPath();
-      return;
-    }
-
-    let crd = coords.shift();
-    let event = {
-      offsetX: crd[0],
-      offsetY: crd[1]
-    };
-
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(event.offsetX, event.offsetY, radius, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(event.offsetX, event.offsetY);
-
-  }, 30)
-
-};
 
 document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 83) {
-    save();
-    
-    console.log('Saved');
-  }
-
-  if (event.keyCode === 82) {
-    console.log('Replaying...');
-
-    coords = JSON.parse(localStorage.getItem('coords'));
-    clear();
-    replay();
-  }
-
   if (event.keyCode === 67) {
-      clear();
-      console.log('Cleared');
+    clear();
   }
 });
 
-fillButton.addEventListener('click', fill)
+fillButton.addEventListener('click', () => {
+  toFocus(fillButton, true);
+  isPressed = true;
+});
 
+canvas.addEventListener('click', () => {
+  if (isPressed) {
+    fill();
+    isPressed = false
+  }
+});
 
+pencil.addEventListener('click', drawPencil);
+brush.addEventListener('click', drawBrush);
+eraser.addEventListener('click', toClean);
