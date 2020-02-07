bd7e47ba353904f42c2c36082fee1b63261fca01
LightDev1 <karumasanmy@gmail.com>
2020-02-08 00:49:58 +0400

    Finally completed

--- script.js
+++ script.js
@@ -6,12 +6,27 @@ const canvas = document.getElementById('canvas'),
     eraser = document.querySelector('.eraser'),
     currentColor = document.querySelector('.current__color'),
     pencil = document.querySelector('.pencil'),
-    brush = document.querySelector('.brush');
+    brush = document.querySelector('.brush'),
+    text = document.querySelector('.text'),
+    toolbar = document.querySelector('.toolbar'),
+    textInput = document.querySelector('.text__input'),
+    acceptBtn = document.querySelector('.accept'),
+    circle = document.querySelector('.circle'),
+    squart = document.querySelector('.squart'),
+    triangle = document.querySelector('.triangle'),
+    settings = document.querySelector('.settings'),
+    width = document.querySelector('.width'),
+    height = document.querySelector('.height');
+
     
 let isMouseDown = false;
 let color = 'black';
 let isPressed = false;
 let isCleanPressed = false;
+let isTextPressed = false;
+let isCirclePressed = false;
+let isSquartPressed = false;
+let isTrianglePressed = false;
 let cleanColor = 'white';
 let prevElement;
 
@@ -71,7 +86,8 @@ const fill = () => {
 };
 
 const toClean = () =>  {
- isCleanPressed = true;
+  input.value = 5;
+  isCleanPressed = true;
   showCurrentColor();
   toFocus(eraser, true);
 };
@@ -118,6 +134,79 @@ const clear = () => {
   ctx.fillStyle = color;
 };
 
+const saveValue = () => {
+  if (textInput.value) {
+    let value = textInput.value;
+    textInput.style.display = '';
+    acceptBtn.style.display = '';
+    return value; 
+  }
+
+  textInput.style.display = '';
+  acceptBtn.style.display = '';
+};
+
+const showText = () =>  {
+   textInput.style.display = 'block';
+   acceptBtn.style.display = 'block';
+   input.value = 16;
+};
+
+const renderText = (event) => {
+
+    let value = saveValue();
+    let radius = input.value;
+
+    if (value) {
+      ctx.fillStyle = color;
+      ctx.font = `${radius}px Arial`;
+      ctx.fillText(value, event.offsetX, event.offsetY);
+    }
+};
+
+const showSettings = () => {
+  settings.style.display = 'block';
+};
+
+const drawCircle = (event) => {
+  isCleanPressed = false;
+
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.arc(event.offsetX, event.offsetY, input.value, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.beginPath();
+};
+
+const drawSquart = (event) => {
+  isCleanPressed = false;
+
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.strokeRect(event.offsetX, event.offsetY, width.value, height.value);
+  ctx.beginPath();
+};
+
+const drawTriangle = (event) => {
+  isCleanPressed = false;
+
+  let radius = input.value;
+
+  console.log(event.offsetX);
+  console.log(event.offsetY);
+
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+  ctx.lineTo(event.offsetX - radius * radius, event.offsetY + radius * radius);
+  ctx.lineTo(event.offsetX + radius * radius,  event.offsetY + radius * radius);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.beginPath();
+};
 
 document.addEventListener('keydown', (event) => {
   if (event.keyCode === 67) {
@@ -130,13 +219,59 @@ fillButton.addEventListener('click', () => {
   isPressed = true;
 });
 
-canvas.addEventListener('click', () => {
+text.addEventListener('click', () => {
+  toFocus(text, true);
+  isTextPressed = true;
+  isCleanPressed = false;
+  showText();
+});
+
+circle.addEventListener('click', () => {
+  toFocus(circle, true);
+  isCirclePressed = true;
+});
+
+squart.addEventListener('click', () => {
+  toFocus(squart, true);
+  showSettings();
+  isSquartPressed = true;
+});
+
+triangle.addEventListener('click', () => {
+  toFocus(triangle, true);
+  isTrianglePressed = true;
+});
+
+canvas.addEventListener('click', (event) => {
   if (isPressed) {
     fill();
-    isPressed = false
+    isPressed = false;
   }
+
+  if (isTextPressed) {
+    renderText(event);
+    isTextPressed = false;
+  }
+
+  if (isCirclePressed) {
+    drawCircle(event);
+    isCirclePressed = false;
+  }
+
+  if (isSquartPressed) {
+    drawSquart(event);
+    isSquartPressed = false;
+    settings.style.display = '';
+  }
+
+  if (isTrianglePressed) {
+    drawTriangle(event);
+    isTrianglePressed = false;
+  }
+
 });
 
 pencil.addEventListener('click', drawPencil);
 brush.addEventListener('click', drawBrush);
 eraser.addEventListener('click', toClean);
+acceptBtn.addEventListener('click', saveValue);
